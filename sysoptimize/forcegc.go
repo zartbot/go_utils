207b1dcc83c33592ae9a22702af7d35a788dc7a0
kevin <ogotaiking@gmail.com>
2019-03-01 16:56:56 +0800

    remove log

--- sysoptimize/forcegc.go
+++ sysoptimize/forcegc.go
@@ -3,14 +3,12 @@ package sysoptimize
 import (
 	"runtime/debug"
 	"time"
-
-	"github.com/sirupsen/logrus"
 )
 
 func ForceGC(t time.Duration) {
 	for {
 		tc := time.After(time.Millisecond * t)
-		logrus.Info("Optimized GC.....")
+		//logrus.Info("Optimized GC.....")
 		debug.FreeOSMemory()
 		<-tc
 	}
